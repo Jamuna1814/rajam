3d9cf3c92ad3b8d6a2270053bac9d08198b5ad4d
Jamuna1814 <66834587+Jamuna1814@users.noreply.github.com>
2020-06-14 21:14:18 +0530

    Add files via upload

---
+++ Basics/Addition.java
@@ -0,0 +1,9 @@
+public class Addition{
+public static void main(String args[]){
+int a,b,s;
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+s=a+b;
+System.out.println(" The sum of "+a+" and "+b+" is "+s);
+}
+}
