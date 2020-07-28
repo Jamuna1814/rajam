2d9784dc4e6ef2c820aa18ec6f6476e0e2b7a4c2
Jamuna1814 <66834587+Jamuna1814@users.noreply.github.com>
2020-07-28 16:07:44 +0530

    Add files via upload

---
+++ prp junit/TestSuiteAll.java
@@ -0,0 +1,10 @@
+package com.wipro.test;
+import java.util.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.wipro.task.TestStringConcat;
+@RunWith(Suite.class)
+@Suite.SuiteClasses(TestStringConcat.class)
+public class TestSuiteAll {
+}
